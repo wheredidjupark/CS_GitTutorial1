25df86203251302aa4bf61e79a9e849f722f3f4f
Ju Hyeon Park <wheredidjupark@gmail.com>
2014-11-25 22:36:57 -0800

    added contact function

--- demo.cpp
+++ demo.cpp
@@ -3,10 +3,18 @@
 //We will use the following program to demonstrate how Git's branching, cloning, merging, and pulling functionality.
 
 using namespace std;
+
+void contact();
+
 int main()
 {
 
 cout << "hello world!\0";
 
 return 0;
+}
+
+void contact()
+{
+    cout << "My name is Ju Park." <<endl <<"Here is my pseudo-contact information: \0 P:310-111-1111";
 }
