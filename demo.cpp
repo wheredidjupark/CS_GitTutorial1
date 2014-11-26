494108351dcfb542a55548b6a7926df0866b3f98
Ju Hyeon Park <wheredidjupark@gmail.com>
2014-11-25 22:26:42 -0800

    initial commit

---
+++ demo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+//We will use the following program to demonstrate how Git's branching, cloning, merging, and pulling functionality.
+
+using namespace std;
+int main()
+{
+
+cout << "hello world!\0";
+
+return 0;
+}
