60346feec4bb165ff82ed06c18cdbbd42ef56cc4
Ju Hyeon Park <wheredidjupark@gmail.com>
2014-11-25 22:32:16 -0800

    index function declared

--- demo.cpp
+++ demo.cpp
@@ -3,10 +3,18 @@
 //We will use the following program to demonstrate how Git's branching, cloning, merging, and pulling functionality.
 
 using namespace std;
+
+void index();
+
 int main()
 {
 
 cout << "hello world!\0";
 
 return 0;
+}
+
+void index()
+{
+    cout <<"this is index"
 }
